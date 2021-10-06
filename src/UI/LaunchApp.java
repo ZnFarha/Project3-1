b86bf91ff7939aeaff35e81646d1fdf3d3fc96cc
Zain Farhah <Zeinfarha123@gmail.com>
2021-10-06 14:57:06 +0200

    Some Changes

--- src/UI/LaunchApp.java
+++ src/UI/LaunchApp.java
@@ -11,8 +11,8 @@ public class LaunchApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LaunchApp.class.getResource("CheckBoxPage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 580, 370);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 900, 1000);
+        stage.setTitle("BabyWatcher!");
         stage.setScene(scene);
         stage.show();
     }
