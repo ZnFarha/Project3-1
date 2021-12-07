0d918fd754422bd07eff845e87e61962b0c9738c
EmeryK-1 <56229024+EmeryK-1@users.noreply.github.com>
2021-12-07 15:58:03 +0100

    HOG implementation

--- src/HOG/Classify.py
+++ src/HOG/Classify.py
@@ -33,14 +33,43 @@ def analyse_hog(row, svms):
 
 
 def analyse_svm_results(df, threshold):
-    df['highest'] = df.drop(['x', 'y', 'hog', 'window_size', 'window'], axis=1).idxmax(axis=1)
-    df['highest_pred'] = df.drop(['x', 'y', 'hog', 'window_size', 'window'], axis=1).max(axis=1)
-    df['best_none'] = 1 - df.drop(['x', 'y', 'hog', 'window_size', 'window'], axis=1).min(axis=1)
+    p = df.drop(['x', 'y', 'hog', 'window_size', 'window'], axis=1)
+    df['highest'] = p.idxmax(axis=1)
+    df['highest_pred'] = p.max(axis=1)
+    # df['best_none'] = 1 - p.min(axis=1)
     df = df[df['highest_pred'] >= threshold]
+    df = df.sort_values('highest_pred', ascending=False).drop_duplicates(['highest'])
 
     if stats: print('{} features found?'.format(len(df)))
 
     return df
+
+
+def iou_value(row, row1, boxes=False):
+    if boxes:
+        boxA = row
+        boxB = row1
+    else:
+        c, c1 = row[['x','y']], row1[['x','y']]
+        boxA = [c[0], c[1], c[0]+row['window_size'][0], c[1]+row['window_size'][1]]
+        boxB = [c1[0], c1[1], c1[0]+row1['window_size'][0], c1[1]+row1['window_size'][1]]
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    # return the intersection over union value
+    return iou
 
 
 def draw_boxes(image, features_found):
@@ -53,10 +82,10 @@ def draw_boxes(image, features_found):
         end = (row['y'] + row['window_size'][1], row['x'] + row['window_size'][0])
         color = (
             255 - 255 * interp(row['highest_pred'],
-                               [features_found['highest_pred'].min(), features_found['highest_pred'].max()],
+                               [0.95, 1],
                                [0, 1]),
             255 - 255 * (1 - interp(row['highest_pred'],
-                                    [features_found['highest_pred'].min(), features_found['highest_pred'].max()],
+                                    [0.95, 1],
                                     [0, 1])), 0)
         img_rect = cv2.rectangle(img_rect, start, end, color, 2)
         img_rect = cv2.putText(img_rect, row['highest'], (row['y'], row['x'] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
@@ -65,8 +94,15 @@ def draw_boxes(image, features_found):
     return img_rect
 
 
-def analyse_image(image, svm_models, crop_thresh=0.2, blur_size=10, bright_strength=2, sharp_strength=2,
-                  square_step=True, windows=[(64, 64), (128, 128)], window_stride=16, pred_threshold=0.9):
+def analyse_image(image, svm_models=None, crop_thresh=0.2, blur_size=10, bright_strength=1, sharp_strength=1,
+                  square_step=True, windows=[(64, 64), (96, 96), (128, 128), (192, 192)], window_stride=16, pred_threshold=0.99):
+    if svm_models is None:
+        from os.path import dirname
+        import Classify
+        resources_path = dirname(__file__)[:-7] + 'resources'
+        # load model
+        svm_models = Classify.load_svm(resources_path + '/SVM')
+
     # 1. Process image and extract HOGs
     processed = Process.process(image, crop_thresh, blur_size, bright_strength, sharp_strength,
                                 square_step)
@@ -98,7 +134,6 @@ if __name__ == "__main__":
     os.chdir('..')
     os.chdir('../resources')
     pwd = os.getcwd()
-
     # 1. Load image(s)
     img_name = random.choice(os.listdir(pwd + '/images/1'))
     print(img_name)
@@ -109,12 +144,7 @@ if __name__ == "__main__":
     print(classifiers)
 
     # 3. Analyse image
-    wanted_classifiers = ['head', 'spine']  # The keys you want
-    wanted_classifiers = dict((k, classifiers[k]) for k in wanted_classifiers if k in classifiers)
-    feat_image, _, _ = analyse_image(img, wanted_classifiers, crop_thresh=0.2, blur_size=10, bright_strength=2,
-                                     sharp_strength=2,
-                                     square_step=True, windows=[(32, 32), (64, 64), (96, 96), (128, 128)],
-                                     window_stride=32)
+    feat_image, _, _ = analyse_image(img)
 
     ####################
     #####
--- src/Preprocessing/Main.py
+++
@@ -1,8 +0,0 @@
-import BasicFunctions
-import ImageCropper
-
-
-image_folder = BasicFunctions.resources_path+"/1"
-csv_file = BasicFunctions.resources_path+"/emery-labels-1.csv"
-
-ImageCropper.crop_from_csv(csv_file, image_folder, False, False, True)
